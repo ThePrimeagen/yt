d8369dcd3d09a7f476050c0bd50645b64417c528
Cody Arnold <carnold@bluebeam.com>
2020-10-03 15:36:46 -0500

    Implement DoublyLinked

--- ds/linked_list/src/linked.ts
+++ ds/linked_list/src/linked.ts
@@ -1,6 +1,7 @@
 export interface DataNode<T> {
     data: T;
     next: DataNode<T> | null;
+    prev: DataNode<T> | null;
 }
 
 export interface IQueue<T> {
@@ -107,12 +108,11 @@ export class Stack<T> implements IStack<T> {
             node.next = this.tail;
             this.tail = node;
         }
-
         ++this._size;
     }
 
     pop(): T | null {
-        if (this._size == 0) {
+        if (this._size === 0) {
             return null;
         }
 
@@ -124,10 +124,147 @@ export class Stack<T> implements IStack<T> {
     }
 
     peek(): T | null {
-        if (this._size == 0) {
+        if (this._size === 0) {
             return null;
         }
 
         return this.tail.data;
+    }
+}
+
+export class DoublyLinked<T> implements IDoublyLinked<T> {
+    private _size: number;
+    private head: DataNode<T> | null;
+    private tail: DataNode<T> | null;
+
+    constructor() {
+        this._size = 0;
+    }
+
+    size(): number { 
+        return this._size; 
+    }
+
+    enqueue(data: T): void { 
+        const node: DataNode<T> = {
+            data,
+            next: null,
+            prev: null
+        };
+
+        if (this.head === null) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = this.tail.next;
+        }
+        ++this._size;
+    }
+
+    dequeue(): T | null {
+        if (this._size === 0) {
+            return null;
+        }
+
+        const item = this.head.data;
+        this.head = this.head.next;
+        this.head.prev = null;
+        --this._size;
+
+        return item;
+    }
+
+    push(data: T): void { 
+        const node: DataNode<T> = {
+            data,
+            next: null,
+            prev: null
+        };
+
+        if (this.tail === null) {
+            this.tail = node;
+        } else {
+            this.tail.next = node
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        ++this._size;
+    }
+
+    pop(): T | null {
+        if (this._size === 0) {
+            return null;
+        }
+
+        const item = this.tail.data;
+        this.tail = this.tail.prev;
+        this.tail.next = null;
+        --this._size;
+
+        return item;
+    }
+
+    getAt(index: number): T { // O(n)
+        return this.getNodeAtIndex(index);
+    }
+
+    insertAt(index: number, data: T): void {
+        const node: DataNode<T> = {
+            data,
+            next: null,
+            prev: null
+        }
+
+        const rightNode = this.getNodeAtIndex(index);
+
+        if (rightNode.prev) {
+            node.prev = rightNode.prev; 
+            rightNode.prev.next = node;
+        }
+
+        rightNode.prev = node;
+        node.next = rightNode;
+    }
+
+    removeAt(index: number): T {
+        if (this._size === 0) {
+            return null;
+        }
+
+        const node = this.getNodeAtIndex(index);
+        if (node.next) {
+            node.next.prev = node.prev;
+        }
+        if (node.prev) {
+            node.prev.next = node.next;
+        }
+
+        return node.data;
+    }
+
+    private getNodeAtIndex(index: number): DataNode {
+        let node: DataNode;
+        if (index < this._size / 2) {
+            node = this.head;
+            for (let i = 0; i < index; ++i) {
+                node = node.next;
+            }
+        } else {
+            node = this.tail;
+            for (let i = this._size - 1; i > index; --i) {
+                node = node.prev;
+            }
+        }
+
+        return node;
+    }
+
+    peek(): T | null {
+        if (this._size === 0) {
+            return null;
+        }
+
+        return this.head.data;
     }
 }
